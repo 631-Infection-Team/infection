4c9fee2b53a48f8bd96104743a16cdfca7897600
George Butler <georgebutleriv@gmail.com>
2020-02-12 20:28:09 -0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Infection
+{
+    public class Player : NetworkBehaviour
+    {
+        CharacterController characterController;
+
+        [Header("Movement")]
+        public float speed = 6.0f;
+
+        void Start()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        void Update()
+        {
+            if (!isLocalPlayer) return;
+
+            Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), Physics.gravity.y * Time.deltaTime, Input.GetAxis("Vertical")) * speed;
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
