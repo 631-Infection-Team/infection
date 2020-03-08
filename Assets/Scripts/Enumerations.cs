444b1528e817cd844d0d3003baf16eb7adcc1fe3
George Butler <georgebutleriv@gmail.com>
2020-03-07 17:21:19 -0800

    Added main menu, FPS controller, networking.

---
+++ Assets/Scripts/Enumerations.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Enumerations
+{
+    Round_PreGame,
+    Round_ActiveGame,
+    Round_PostGame,
+    Round_Waiting
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Infection
+{
+    public class PlayerController : NetworkBehaviour
+    {
+        CharacterController characterController;
+        Transform groundTrigger;
+        float groundDistance = 0.4f;
+        Vector3 velocity;
+        bool isGrounded;
+
+        [Header("Movement")]
+        public float speed = 12.0f;
+        public LayerMask groundMask;
+
+        void Start()
+        {
+            characterController = GetComponent<CharacterController>();
+            groundTrigger = gameObject.transform.Find("Ground Trigger").transform;
+        }
+
+        void Update()
+        {
+            isGrounded = Physics.CheckSphere(groundTrigger.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 moveDirection = transform.right * horizontal + transform.forward * vertical;
+            characterController.Move(moveDirection * speed * Time.deltaTime);
+
+            // Gravity
+            velocity.y = velocity.y + Physics.gravity.y * Time.deltaTime;
+            characterController.Move(velocity * Time.deltaTime);
+        }
+    }
+}
