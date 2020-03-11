ad5ba0cbdd6b8440c51705d55ab1762bfb257c5b
George Butler <georgebutleriv@gmail.com>
2020-03-11 14:01:34 -0700

    Lobby networking

---
+++ Assets/Scripts/PlayerGame.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGame : NetworkBehaviour
+{
+    [SyncVar]
+    public int index;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}
