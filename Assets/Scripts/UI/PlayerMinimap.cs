695b65bfb8a46b3ccb6c578179236da1ee1589e1
George Butler <georgebutleriv@gmail.com>
2020-04-13 18:36:09 -0700

    Update

--- Assets/Scripts/UI/PlayerMinimap.cs
+++ Assets/Scripts/UI/PlayerMinimap.cs
@@ -10,25 +10,14 @@ namespace _UI
         // [SerializeField] private Material allyMarker = null;
         [SerializeField] private Material enemyMarker = null;
 
-        public override void OnStartLocalPlayer()
-        {
-            base.OnStartLocalPlayer();
-
-            minimapMarker.SetActive(true);
-        }
-
         public override void OnStartClient()
         {
             base.OnStartClient();
 
-            if (isLocalPlayer)
-            {
-                minimapMarker.GetComponent<MeshRenderer>().material = playerMarker;
-            }
-            else
-            {
-                minimapMarker.GetComponent<MeshRenderer>().material = enemyMarker;
-            }
+            minimapMarker.GetComponent<MeshRenderer>().material = isLocalPlayer ? playerMarker : enemyMarker;
+
+            // This needs to be turned on for all players, not only local player
+            minimapMarker.SetActive(true);
         }
     }
 }
