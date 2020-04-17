e0230b5be97eda08a14d4831385707c9720686be
Jian Qi <jianqi1997@gmail.com>
2020-04-16 23:27:44 -0700

    Fade in between HomePage to 2nd Scene

--- Assets/Scripts/Fade_In_Scene.cs
+++ Assets/Scripts/Fade_In_Scene.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿/*
+*   This script is used between 01_Menu and 02_Prototype
+*   Fade In from white to imitate Scene transition. 
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Fade_In_Scene : MonoBehaviour
-{
+public class Fade_In_Scene : MonoBehaviour{
+    public Image White_Fade_In;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        White_Fade_In.canvasRenderer.SetAlpha(1.0f);
+        fade_in_scene();
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void fade_in_scene(){
+        White_Fade_In.CrossFadeAlpha(0, 3, false);
     }
 }
