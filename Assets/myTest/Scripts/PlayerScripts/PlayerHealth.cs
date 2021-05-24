030546a3444fd34e3637db3b18787ee39e7e3bc5
malissam <memurga@gmail.com>
2021-05-24 07:59:37 -0700

    Characters able to shoot zombies

---
+++ Assets/myTest/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace myTest{
+public class PlayerHealth : MonoBehaviourPun
+{
+
+    //public float health = 3;
+
+    [SerializeField] private Player1 player = null;
+    private void Start()
+    {
+        player = GetComponent<Player1>();
+        //player.SetDefaults();
+    }
+    
+
+
+
+}
+
+}
